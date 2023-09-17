d578f937343a3e922bf8f745496dce6e952b09d3
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-09-17 08:05:58 +0000

    my Javascript series start here

---
+++ JavaScript_files/Lecture01.js
@@ -0,0 +1,22 @@
+//mode strict//
+
+// datatypes
+
+// primetvetype
+
+// 1 Number
+// 2 boolean
+// 3 Bigint
+// 4 Null
+// 5 Undefined
+// 6 String
+// 7 Symbole
+
+// and non PrimetiveDatetype
+
+// 8 Object
+
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
---
+++ JavaScript_files/Lecture02.js
@@ -0,0 +1,40 @@
+//let score = "33 abs"
+//let score = null
+//let score = undefined
+// let score = true
+let score = false
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+//let value = 1
+//let value = 0
+//let value = ""
+let value = "Amit"
+
+let changeInBoolean = Boolean(value);
+//console.log(changeInBoolean);
+
+
+
+
+// ***************************************************Opration of Datatypes***************************************************
+
+
+let sunvalue = 3;
+
+let negvalue = -sunvalue
+console.log(negvalue);
+
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  //plesae use parnthasis ( )
+console.log(1 + 2 + "2");
