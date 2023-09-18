e2cf5d30a736b8d3e60fe14b736f5852e307a854
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-09-18 07:54:55 +0000

    stack and heap

--- JavaScript_files/lecture03.js
+++ JavaScript_files/lecture03.js
@@ -5,3 +5,31 @@
 //Reference  {non-primitive datatypes}
 
 //Arrey, Object, functions
+
+//**********stack**********
+
+
+let myName = "Amit sharma";
+
+let anotherName = myName
+
+anotherName = "antim";
+
+
+console.log(myName);
+console.log(anotherName);
+
+//**********Heap**********
+
+const userOne = {
+      email: 'as245666@gmail.com',
+      upi: 'fhsdfdfgfgh'
+};
+
+
+const userTwo = userOne
+
+userTwo.email = "aswe23.fgu";
+
+console.log(userOne);
+console.log(userTwo);
