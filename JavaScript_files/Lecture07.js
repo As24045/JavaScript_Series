f49851052b38dfff35abce5d1a9b2eddd5671d85
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-09-23 06:20:43 +0000

    basic of Array

--- JavaScript_files/Lecture07.js
+++ JavaScript_files/Lecture07.js
@@ -1,5 +1,40 @@
 //Arrays
 
-const firstArr = [1,2,3,4,5];
+const firstArr = [7,3,4,4,5];
 
-console.log(firstArr[2]);
+//console.log(firstArr[2]);
+
+//Array method
+
+firstArr.push(6);                   //add the first value of array                            
+//console.log(firstArr)
+firstArr.pop();   
+firstArr.pop();                    //remove the last value of array
+//console.log(firstArr)
+
+firstArr.unshift(7)             //Add the element in the starting of arry             
+firstArr.shift()                   //remove the element in the starting of arry             
+//console.log(firstArr);
+
+
+// console.log(firstArr.includes(30))
+// console.log(firstArr.indexOf(30))
+// console.log(firstArr.indexOf(3))
+
+const newArray = firstArr.join()   //join method changes to array to string
+//console.log(firstArr)
+//console.log(newArray)
+
+console.log(firstArr)
+
+const newArray2 = firstArr.slice(1,3)
+
+console.log(newArray2)
+
+
+console.log(firstArr)
+
+const newArray3 = firstArr.splice(1,3)
+console.log(firstArr)
+console.log(newArray3)
+
