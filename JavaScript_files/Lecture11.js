54cefef985811db658831bae3418045c019ca971
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-09-27 04:39:16 +0000

    function in array

--- JavaScript_files/Lecture11.js
+++ JavaScript_files/Lecture11.js
@@ -1,8 +1,8 @@
 function sayMyName(){
-    console.log("A")
-    console.log("m")
-    console.log("i")
-    console.log("t")
+    // console.log("A")
+    // console.log("m")
+    // console.log("i")
+    // console.log("t")
 }
 
 //sayMyName                                      // we call him Refrence
@@ -20,7 +20,7 @@ function addTwoNumber(number1, number2) {              //wherw number 1 and numb
     
 }
 const result = addTwoNumber(2,4)
-console.log(result)                             // when we will called a function when we will call him argument
+//console.log(result)                             // when we will called a function when we will call him argument
 
 
 
@@ -29,4 +29,40 @@ return `${username} is loged in now`
 
 }
 
-console.log(logedInPerson("Amit"))
+//console.log(logedInPerson("Amit"))
+
+
+function calculateCardPrice(val1, val2, ...num1) {
+
+    return num1
+}
+
+//console.log(calculateCardPrice(200, 300, 400));
+
+const user = {
+    username : "amit",
+    price : 199
+}
+
+function hendalObject(anyObject) {
+
+    console.log(`Username is ${anyObject.username} and price ${anyObject.price}`);     
+}
+
+//hendalObject(user)
+
+hendalObject ({
+    username : "satyam",
+    price : "399"
+})
+
+const myNewArrey = [200, 300, 400, 500];
+
+function returnSecondValue(second) {
+
+    return second[1]
+    
+}
+
+//console.log(returnSecondValue(myNewArrey));   //or
+console.log(returnSecondValue([200, 900, 400, 500]));
