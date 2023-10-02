6c163f14c90a9a47483f6badd6de7843384c66c0
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-10-02 07:05:04 +0000

    while loop and Do while loop

---
+++ JavaScript_files/17_while.js
@@ -0,0 +1,28 @@
+// while loops
+
+let i = 0;
+while (i <= 10) {
+    //console.log(`index value of ${i}`)
+    i = i + 2;
+}
+
+let myArray = [ 'amit', 'satyam', 'shyam'];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+    //console.log(`${myArray[arr]}`);
+    arr = arr + 1;
+    
+}
+
+
+//Do while loop *********************************************************************************
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+    
+} while (score <= 10);
